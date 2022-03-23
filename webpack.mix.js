22f32434449c122ba2cba23852178057d44e04fd
ニャンモーアウン <40397836+nyanmoeaung@users.noreply.github.com>
2022-03-23 14:14:56 +0630

    disabled laravel mix notifications

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css')
+    .disableNotifications()
     .webpackConfig(require('./webpack.config'));
 
 if (mix.inProduction()) {
